eba397aeb0107eed5a3f91468adc482c04a8a4a2
Kishan Gupta <unique.kishan.gupta@gmail.com>
2019-12-21 14:27:08 +0530

    Setting of router done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const port = 5000;
 // express function is a top level function exported by expresss module.
 const app=express();
-
+const port = 5010;
+// we need to use that router function created in Router folder , so we import that file in  our main index.js file
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+// setting up Routes.
+
+const express=require('express');
+//we need Router function from express module.
+const router= express.Router();
+
+console.log('router loaded ');
+
+module.exports=router;
