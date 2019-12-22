ad233dd9c302419b9ebb00cf4965fa1003527b9f
Kishan Gupta <unique.kishan.gupta@gmail.com>
2019-12-22 15:33:08 +0530

    Added users controller

---
+++ controllers/users_controllers.js
@@ -0,0 +1,4 @@
+// This controller file can control many users. 
+module.exports.profile=function(req,res){
+    res.end('<h1>Users Profile </h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,9 @@ const homeController=require('../controllers/home_controller');
 console.log('router loaded ');
 //this is a route we have set for the homeController
 router.get('/',homeController.home);
+//ROuter will use 'users.js' file which is present in router folder for /users url.
+//syntax 
+// router.use('/routerName',require('./routerFile'))
+router.use('/user',require('./users'));
 
 module.exports=router;
